8a545345ec9451efaa10086ae8d0fa75d3222723
Dhevarajan_A <dhevarajan.alaguraju@netmeds.com>
2024-08-11 10:58:10 +0530

    Component changes

--- src/main/java/com/generic/crud/entity/BaseEntity.java
+++ src/main/java/com/generic/crud/entity/BaseEntity.java
@@ -1,14 +1,16 @@
 package com.generic.crud.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Entity
+//@Datamvn
+//@NoArgsConstructor
+//@AllArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
 
     public int id;
--- src/main/java/com/generic/crud/repository/BaseRepository.java
+++ src/main/java/com/generic/crud/repository/BaseRepository.java
@@ -2,11 +2,13 @@ package com.generic.crud.repository;
 
 import com.generic.crud.entity.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Repository
-@Transactional
+//@Repository
+//@Transactional
+@Component
 public interface BaseRepository extends JpaRepository<BaseEntity, Integer> {
 
 }
